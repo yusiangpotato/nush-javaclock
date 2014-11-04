164a836ab21ac1485121cbdbfe699e433fdd20a5
eliot.lim <eliot.lim@gmail.com>
2014-11-04 23:35:51 +0800

    The refactor process didn't like me renaming stuff.

--- src/appname/gui/ClockPanel.java
+++ src/appname/gui/ClockPanel.java
@@ -17,7 +17,7 @@ public class ClockPanel extends JPanel {
     @Override
     public void paintComponent(Graphics g){//Repaint every second.
         //super.paintComponent(g);
-        this.setSize(Util.safeLongToInt(Math.round(2 * size)), Util.safeLongToInt(Math.round(2 * size)));
+        //this.setSize(Util.safeLongToInt(Math.round(2 * size)), Util.safeLongToInt(Math.round(2 * size)));
         {//Setup the clock face markings
             for(int i=0; i<60;i++){
                 double theta = Util.map(i, 0, 60, 2 * Math.PI, 0);
@@ -30,6 +30,7 @@ public class ClockPanel extends JPanel {
                 if(i%5==0){
                     int posX = Util.safeLongToInt(Math.round(Util.PolarToCartesianX(theta, 0.72 * size) + size * 0.92));
                     int posY = Util.safeLongToInt(Math.round(Util.PolarToCartesianY(theta, 0.72 * size) + size * 1.05));
+	                String s= ""+(i==0?12+"":(i/5<10?" "+i/5:i/5)) ;
                     g.drawString((i == 0 ? 12 + "" : (i / 5 < 10 ? " ":"" +i)),posX ,posY );
                     
                 }
