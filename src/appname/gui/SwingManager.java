f13840438f4490134f85fecb11648f67a9c30dab
yusiangpotato <yusiang1998@gmail.com>
2014-11-06 23:11:12 +0800

    Working V0.3 TODO Proper event pane

--- src/appname/gui/SwingManager.java
+++ src/appname/gui/SwingManager.java
@@ -10,7 +10,7 @@ import java.io.InputStreamReader;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
-
+//
 /**
  * Created by yusiang on 11/4/14.
  */
