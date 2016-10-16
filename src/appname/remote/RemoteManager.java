dd393e2b264a1e751ef2f06208b0917d16d72b61
Yusiang <yusiang1998@gmail.com>
2016-10-17 00:54:28 +0800

    Working remote manager implementation

--- src/appname/remote/RemoteManager.java
+++ src/appname/remote/RemoteManager.java
@@ -6,6 +6,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -17,12 +19,25 @@ import java.util.concurrent.TimeUnit;
 public class RemoteManager implements Runnable {
     ScheduledExecutorService ExecService;
     final JButton toiletButton;
-    int state = 0b00000100; //0b00000CBA C=Uninit B=Local A=1out
+    int state = 0b00000100; //0b0000DCBA D=UDPfail C=Uninit B=Local A=1out
+    int oldState = 0xFF;
+
+    int missedCounter=0;
+    final int missedThresh = 20;//~1secs
+
+    final int UDPport=2302;
+    DatagramSocket socket=null;
 
     public RemoteManager(JButton button) {
         this.toiletButton =button;
+        try{
+            socket = new DatagramSocket(UDPport);
+            socket.setSoTimeout(45);
+        }catch(Exception e){
+            state |= 0b00001000;//UDP failure
+            System.out.println("Unable to bind port 2302, no remote control function available.");
+        }
         SwingUtilities.invokeLater(new Runnable() { //Do we really need this? Invokation is guaranteed from swing.
-
             public void run() {
                 toiletButton.setBackground(new Color(154, 154, 154));
                 toiletButton.setFont(new Font("Sans",Font.PLAIN,40));
@@ -31,17 +46,20 @@ public class RemoteManager implements Runnable {
                 toiletButton.addMouseListener(new MouseListener() {
                     @Override
                     public void mouseClicked(MouseEvent e) {
-                        if(e.getButton() == MouseEvent.BUTTON1) {
+                        if(SwingUtilities.isLeftMouseButton(e)) {
+                            //System.out.println("LMOUSE");
                             state &= 0b11111011; //Set to manual mode
                             state |= 0b00000010; //Set to manual mode
                             state ^= 0b00000001; //Toggle current display
 
-                        }else if(e.getButton()==MouseEvent.BUTTON2){
-                            state = 0b00000100; //Set to auto, uninitialized mode
+                        }else if(SwingUtilities.isRightMouseButton(e)){
+                            //System.out.println("RMOUSE");
+                            state &= 0b11111101; //Auto mode
+                            state |= 0b00000100; //uninitialized mode
 
                         }
 
-                        updateButton(state);
+                        updateButton();
 
                     }
 
@@ -63,29 +81,59 @@ public class RemoteManager implements Runnable {
 
     @Override
     public void run() {
-        //TODO receive, parse, decode UDP packet. Update state.
-        if((state&=0b00000010)!=0) return; //Manual mode. No touchie!
-        else updateButton(state); //We update button
+        byte[] b = new byte[100];
+        DatagramPacket p = new DatagramPacket(b,100);
+        if((state & 0b00001000)!=0) return; //Port is not bound
+        try{
+            socket.receive(p);
+            String str = new String(p.getData());
+            //System.out.println(str);
+            String[] strs = str.trim().split("-");
+            if(strs.length<5) return; //Not ours
+            if(!strs[0].equals("JavaClockRemote")) return;//Hmm
+            if(!strs[1].equals("v001")) return;//Hmm
+            if((state&0b00000010)!=0) return; //Manual mode. No touchie!
+            if(Integer.parseInt(strs[4])!=0) state |= 1;
+            else state &= 0xFE;
+            state&=0b11111011;//Initialized
+            missedCounter=0;
+        }catch (Exception e){
+            //e.printStackTrace();
+            missedCounter++;
+            if(missedCounter>missedThresh){
+                missedCounter=missedThresh;
+                state |= 0b00000100;
+            }
+        }
+        if((state&0b00000010)!=0) return; //Manual mode. No touchie!
+        else updateButton(); //We update button
     }
 
-    private void updateButton(int xstate){
+    private void updateButton(){
+        if(state==oldState) return;
+        oldState=state;
+        //System.out.println("St"+ state);
         SwingUtilities.invokeLater(new Runnable() {
 
             public void run() {
-                if((xstate&0b00000100)!=0){
+                if((state&0b00001000)!=0 && (state&0b00000010)==0){ //Fail & auto
+                    toiletButton.setBackground(new Color(255, 47, 252));
+                    toiletButton.setText("<html><p>Failure</p><p style=\"font-size: 16\">Restart program</p></html>");
+                }
+                else if((state&0b00000100)!=0){
                     toiletButton.setBackground(new Color(154, 154, 154));
-                    toiletButton.setText("<html><p>Searching</p><p style=\"font-size: 16\">Remote control</p></html>");
+                    toiletButton.setText("<html><p>Waiting</p><p style=\"font-size: 16\">Remote control</p></html>");
                 }
                 else {
                     String s;
-                    if ((xstate & 0b00000001) != 0) {
+                    if ((state & 0b00000001) != 0) {
                         toiletButton.setBackground(new Color(255, 134, 137));
                         s="<html><p>ONE OUT</p>";
                     } else {
                         toiletButton.setBackground(new Color(134, 255, 136));
                         s="<html><p>ALL IN</p>";
                     }
-                    if((xstate&0b00000010) != 0 ){
+                    if((state&0b00000010) != 0 ){
                         s+="<p style=\"font-size: 16\">Local control</p></html>";
                     }else{
                         s+="<p style=\"font-size: 16\">Remote control</p></html>";
